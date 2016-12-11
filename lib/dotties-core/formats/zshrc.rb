38fba67a7e7a342568f634d253e04037c3c7bb42
Amjith Ramanujam <amjith.r@gmail.com>
2016-12-11 10:19:25 -0800

    Revert "Make postup bash executable."
    This reverts commit 47bcd3b514bc666ae38a03081fce38139117071f.

--- lib/dotties-core/formats/zshrc.rb
+++ lib/dotties-core/formats/zshrc.rb
@@ -1,12 +1,9 @@
 module Formats
   class Zshrc < Adapters::Require
     def template(path)
-%{#!/usr/bin/env bash
-if [ -f #{path} ] 
-then
-    source #{path}
-fi 
-}
+      %{
+        [[ -f #{path} ]] && source #{path}
+      }
     end
   end
 end
